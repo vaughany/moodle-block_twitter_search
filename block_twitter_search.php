ddc80f04ffb40281976d479aed7bb6202b93a104
Kevin Hughes <kevtufc@gmail.com>
2011-11-07 15:40:50 +0000

    Add licence GPL3

--- block_twitter_search.php
+++ block_twitter_search.php
@@ -1,4 +1,22 @@
 <?php
+// This file is part of Twitter Search block for Moodle
+//
+// Twitter Search Block for Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Twitter Search Block for Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Twitter Search Block for Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Copyright Kevin Hughes 2011
+
+
 class block_twitter_search extends block_base{
   public function init(){
     $this->title  = get_string('twitter_search','block_twitter_search');
