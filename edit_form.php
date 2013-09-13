6c1bebb2cae144125bf1d3fb6835ae27ab5317b5
Paul Vaughan <paulvaughan@southdevon.ac.uk>
2013-09-13 14:48:51 +0100

    Whitespace.

--- edit_form.php
+++ edit_form.php
@@ -20,36 +20,36 @@ class block_twitter_search_edit_form extends block_edit_form {
 
     protected function specific_definition($mform) {
         $mform->addElement('header', 'configheader', get_string('blocksettings', 'block'));
-        
+
         $mform->addElement('text', 'config_title_block', get_string('title_block', 'block_twitter_search'));
         $mform->setType('config_title_block', PARAM_MULTILANG);
-        
+
         $mform->addElement('text', 'config_search_term', get_string('search_term', 'block_twitter_search'));
         $mform->setDefault('config_search_term', '#moodle');
         $mform->setType('config_search_term', PARAM_MULTILANG);
-        
+
         $mform->addElement('select', 'config_numtweets', get_string('numtweets', 'block_twitter_search'), range(1, 20));
         $mform->setDefault('config_numtweets', 2);
         $mform->setType('config_numtweets', PARAM_INT);
-        
+
         // New option: tweet types.
         $mform->addElement('select', 'config_tweettype', get_string('tweettype', 'block_twitter_search'), 
             array('mixed' => 'Mixed', 'recent' => 'Recent', 'popular' => 'Popular'));
         $mform->setDefault('config_tweettype', 'recent');
         $mform->setType('config_tweettype', PARAM_MULTILANG);
 
-        $mform->addElement('advcheckbox', 'config_show_names', get_string('show_names', 'block_twitter_search'));
-        $mform->setDefault('config_show_names', true);
-        $mform->setType('config_show_names', PARAM_BOOL);
-        
         $mform->addElement('advcheckbox', 'config_show_usernames', get_string('show_usernames', 'block_twitter_search'));
         $mform->setDefault('config_show_usernames', true);
         $mform->setType('config_show_usernames', PARAM_BOOL);
-        
+
+        $mform->addElement('advcheckbox', 'config_show_names', get_string('show_names', 'block_twitter_search'));
+        $mform->setDefault('config_show_names', true);
+        $mform->setType('config_show_names', PARAM_BOOL);
+
         $mform->addElement('advcheckbox', 'config_show_images', get_string('show_images', 'block_twitter_search'));
         $mform->setDefault('config_show_images', true);
         $mform->setType('config_show_images', PARAM_BOOL);
-        
+
         $mform->addElement('advcheckbox', 'config_show_update', get_string('show_update', 'block_twitter_search'));
         $mform->setDefault('config_show_update', true);
         $mform->setType('config_show_update', PARAM_BOOL);
